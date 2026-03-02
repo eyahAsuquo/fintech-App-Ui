cb2fe973f8227306751a377cc8c64bff29bd3f5c
eyahAsuquo <eyahasuquo@gmail.com>
2026-03-02 13:57:06 +0100

    edited the home page

--- lib/screens/Account_SetUp/account_verification.dart
+++ lib/screens/Account_SetUp/account_verification.dart
@@ -1,4 +1,6 @@
+import 'package:fintech_ui/screens/home/home_page.dart';
 import 'package:fintech_ui/utils/colors/colors.dart';
+import 'package:fintech_ui/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,13 +11,37 @@ class AccountVerificationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: white,
-      body: Center(
-        child: Text(
-          "Coming Soon!",
-          style: GoogleFonts.inter(
-            color: deepBlue,
-            fontWeight: .bold,
-            fontSize: 30,
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            // mainAxisAlignment: .center,
+            // crossAxisAlignment: .center,
+            // mainAxisSize: .min,
+            children: [
+              Spacer(),
+              Text(
+                "Coming Soon!",
+                style: GoogleFonts.inter(
+                  color: deepBlue,
+                  fontWeight: .bold,
+                  fontSize: 30,
+                ),
+              ),
+              Spacer(),
+              Button(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (_) => HomePage()),
+                  );
+                },
+                text: "Home",
+                bgColor: blueColor,
+                fgColor: white,
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -80,13 +80,117 @@ class _HomePageState extends State<HomePage> {
                   width: double.infinity,
                   height: 350,
                   margin: EdgeInsets.only(right: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
                     color: const Color(0x52002D52),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search_outlined, color: Colors.white),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: .end,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.chevron_right_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        child: Icon(Icons.person_2, size: 50),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Your available balance is",
+                        style: GoogleFonts.inter(color: white, fontSize: 12),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "N20,983",
+                        style: GoogleFonts.inter(
+                          color: white,
+                          fontWeight: .bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                        child: Text(
+                          "By this time last month, you spent slightly higher (N22,719)",
+                          style: GoogleFonts.inter(color: white, fontSize: 12),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Column(
+                        spacing: 15.0,
+
+                        children: [
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Text(
+                                "Kuda Bank",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                "N12,000.00",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Text(
+                                "GT Bank",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                "NN950.00",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Text(
+                                "PiggyVest",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                "N1,050.00",
+                                style: GoogleFonts.inter(
+                                  color: white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
                 SizedBox(height: 20),
@@ -94,13 +198,52 @@ class _HomePageState extends State<HomePage> {
                   width: double.infinity,
                   height: 80,
                   margin: EdgeInsets.only(right: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
                     color: const Color(0x52002D52),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search_outlined, color: Colors.white),
+                  child: Row(
+                    // mainAxisSize: .min,
+                    spacing: 20,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: blueColor,
+                        foregroundColor: white,
+                        radius: 20,
+                        child: Icon(Icons.settings),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: .start,
+                          mainAxisSize: .min,
+                          children: [
+                            Text(
+                              "Sort your transactions",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 14,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                            Text(
+                              "Get points for sorting your transactions",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.chevron_right_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 SizedBox(height: 20),
@@ -111,15 +254,208 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(height: 20),
                 Container(
                   width: double.infinity,
-                  height: 80,
+
+                  height: 180,
                   margin: EdgeInsets.only(right: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
                     color: const Color(0xFF3A37FF),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search_outlined, color: Colors.white),
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: .spaceBetween,
+                        children: [
+                          Text(
+                            "You have",
+                            style: GoogleFonts.inter(color: white),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.chevron_right_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      Text(
+                        "N20,983",
+                        style: GoogleFonts.inter(
+                          color: white,
+                          fontWeight: .bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Left out of N80,888 budgeted",
+                        style: GoogleFonts.inter(color: white, fontSize: 12),
+                      ),
+                      SizedBox(height: 20),
+
+                      //add indicator
+                      LinearProgressIndicator(
+                        value: 0.7,
+                        backgroundColor: Color(0x52002D52),
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                      ),
+
+                      SizedBox(height: 20),
+                      Text(
+                        "😱 Sapa go soon catch you bros, calm down!",
+                        style: GoogleFonts.inter(color: white, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Transactions",
+                  style: GoogleFonts.inter(color: white, fontWeight: .w500),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  margin: EdgeInsets.only(right: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF3A37FF),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: .spaceBetween,
+                        children: [
+                          Text(
+                            "Recent Transactions",
+                            style: GoogleFonts.inter(color: white),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.chevron_right_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        spacing: 2,
+                        children: [
+                          ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: const Color(0xFFEFF5EF),
+                              child: Text(
+                                "J",
+                                style: GoogleFonts.inter(
+                                  color: deepBlue,
+                                  fontWeight: .w600,
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              "John Ogaga",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                            subtitle: Text(
+                              "Zenith Bank 12:03 AM",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            trailing: Text(
+                              "+N20,983",
+                              style: GoogleFonts.inter(
+                                color: const Color(0xFFC7F5C8),
+                                fontSize: 12,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                          ),
+
+                          //
+                          ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: const Color(0xFFEFF5EF),
+                              child: Text(
+                                "H",
+                                style: GoogleFonts.inter(
+                                  color: deepBlue,
+                                  fontWeight: .w600,
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              "Habib Yogurt",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                            subtitle: Text(
+                              "GT-Bank 12:03 AM",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            trailing: Text(
+                              "-N20,983",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 12,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                          ),
+                          //
+                          ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: const Color(0xFFEFF5EF),
+                              child: Text(
+                                "C",
+                                style: GoogleFonts.inter(
+                                  color: deepBlue,
+                                  fontWeight: .w600,
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              "Christian Oku",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                            subtitle: Text(
+                              "Zenith Bank 12:03 AM",
+                              style: GoogleFonts.inter(
+                                color: white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            trailing: Text(
+                              "+N100,983",
+                              style: GoogleFonts.inter(
+                                color: const Color(0xFFC7F5C8),
+                                fontSize: 12,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ],
